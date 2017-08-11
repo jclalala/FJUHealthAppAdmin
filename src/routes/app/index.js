132baefbcae37929a618b313ead3a8bb6464b970
Jeffrey Chang <jclalala@gmail.com>
2017-08-11 14:08:46 +0800

    remove a whole bunch of unneeded routes

--- src/routes/app/index.js
+++ src/routes/app/index.js
@@ -3,14 +3,7 @@ module.exports = {
   getChildRoutes(partialNextState, cb) {
     require.ensure([], (require) => {
       cb(null, [
-        require('./routes/charts'),
-        require('./routes/dashboard'),
-        require('./routes/ecommerce'),
-        require('./routes/forms'),
-        require('./routes/pageLayouts'),
-        require('./routes/pages'),
-        require('./routes/tables'),
-        require('./routes/ui'),
+        require('./routes/dashboard')
       ]);
     });
   },
