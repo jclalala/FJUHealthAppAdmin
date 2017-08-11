8426d607081208762aec2f28d8a977ea98562df2
Jeffrey Chang <jclalala@gmail.com>
2017-08-11 14:12:57 +0800

    more stripping...

--- src/routes/app/routes/pageLayouts/routes/centered/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  path: 'centered',
-  getComponent(nextState, cb) {
-    require.ensure([], (require) => {
-      cb(null, require('./components/Centered'));
-    });
-  }
-};
--- src/routes/signUp/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  path: 'sign-up',
-  getComponent(nextState, cb) {
-    require.ensure([], (require) => {
-      cb(null, require('./components/SignUp'));
-    });
-  }
-};
